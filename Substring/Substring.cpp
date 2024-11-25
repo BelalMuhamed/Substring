a88939011b5caa6926964e0da303e3e89f204576
BelalBasal <basdibrahim@gmail.com>
2024-11-25 17:58:39 +0200

    ForcedAnswer

--- Substring/Substring.cpp
+++ Substring/Substring.cpp
@@ -4,9 +4,18 @@
 #include <string>
 
 using namespace std;
-
+void SubStrings(string str)
+{
+    for (int i = 0; i < str.length();i++)
+    {
+        for (int j = 0;j < str.length()-i;j++)
+        {
+            cout << str.substr(i, j+1)<<"\n";
+        }
+    }
+}
 int main()
 {
-    
+    SubStrings("123A");
 }
 
